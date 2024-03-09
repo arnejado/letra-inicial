38125218337405bd163b8dcf17b1f811910b6fc4
Pablo Rodriguez <arnejado@hotmail.com>
2024-03-09 14:03:40 +0100

    empezando con la carpeta js y la base de datos

---
+++ js/bd.js
@@ -0,0 +1,5 @@
+const dibujos = [
+    { id: 1, inicial: "o", url: "../imgenes/001.jpeg"},
+    { id: 2, inicial: "v", url: "../imgenes/002.jpeg"},
+    { id: 3, inicial: "a", url: "../imgenes/003.jpeg"},
+]
