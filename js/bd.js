ac86264cba2c116a3ba22c8a025a33a2059f6ff6
Pablo Rodriguez <arnejado@hotmail.com>
2024-04-03 20:56:00 +0200

    agregando tres animales

--- js/bd.js
+++ js/bd.js
@@ -4,4 +4,7 @@ const bd = [
     { id: 3, inicial: "a", url: "/imagenes/003.jpeg"},
     { id: 4, inicial: "p", url: "/imagenes/004.jpeg"},
     { id: 5, inicial: "l", url: "/imagenes/005.jpeg"},
+    { id: 6, inicial: "z", url: "/imagenes/006.jpeg"},
+    { id: 7, inicial: "f", url: "/imagenes/007.jpeg"},
+    { id: 8, inicial: "d", url: "/imagenes/008.jpeg"},
 ]
