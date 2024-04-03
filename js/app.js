db825924301cddfdb58266f22ece93ad89f8e855
Pablo Rodriguez <arnejado@hotmail.com>
2024-04-03 21:56:53 +0200

    crea botones aleatorios

--- js/app.js
+++ js/app.js
@@ -1,17 +1,39 @@
 var dibujos =  document.getElementById("dibujos");
+var aleatorioMax = bd.length;
+var aleatorioMin = 0;
+const seleccionAnimales = [];
 
-console.log (dibujos);
-console.log(bd[3].url);
+
+crearArraySeleccion (bd, seleccionAnimales);
 
 cargarBotones (); 
+
+
+function crearArraySeleccion (bd, seleccionAnimales) {
+  
+    const idsSeleccionados = new Set();
+
+    while (seleccionAnimales.length < 5) {
+        const indiceAleatorio = Math.floor(Math.random() * bd.length);
+        const animalAleatorio = bd[indiceAleatorio];
+
+        if (!idsSeleccionados.has(animalAleatorio.id)) {
+            seleccionAnimales.push(animalAleatorio);
+            idsSeleccionados.add(animalAleatorio.id);
+        }
+    }
+    
+    console.log(seleccionAnimales);
+    console.log(seleccionAnimales.length);
+}
 
 function cargarBotones () {
 
     dibujos.innerHTML = `
-    <button class="boton1 boton"> <img src="${bd[0].url}" alt="Imagen del boton"> </button>
-    <button class="boton2 boton"> <img src="${bd[1].url}" alt="Imagen del boton"> </button>
-    <button class="boton3 boton"> <img src="${bd[2].url}" alt="Imagen del boton"> </button>
-    <button class="boton4 boton"> <img src="${bd[3].url}" alt="Imagen del boton"> </button>
-    <button class="boton5 boton"> <img src="${bd[4].url}" alt="Imagen del boton"> </button>
+    <button class="boton1 boton"> <img src="${seleccionAnimales[0].url}" alt="Imagen del boton"> </button>
+    <button class="boton2 boton"> <img src="${seleccionAnimales[1].url}" alt="Imagen del boton"> </button>
+    <button class="boton3 boton"> <img src="${seleccionAnimales[2].url}" alt="Imagen del boton"> </button>
+    <button class="boton4 boton"> <img src="${seleccionAnimales[3].url}" alt="Imagen del boton"> </button>
+    <button class="boton5 boton"> <img src="${seleccionAnimales[4].url}" alt="Imagen del boton"> </button>
     `
 }
