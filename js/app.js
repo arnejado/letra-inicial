bdaf7363053d73461c702783a7adb632766127b9
Pablo Rodriguez <arnejado@hotmail.com>
2024-04-04 16:36:47 +0200

    ya reconcoce fallos

--- js/app.js
+++ js/app.js
@@ -4,30 +4,17 @@ var aleatorioMax = bd.length;
 var aleatorioMin = 0;
 const seleccionAnimales = [];
 var letraPantalla = ""
-let letraSeleccionada = "";
+var letraSeleccionada = "";
 const botones = document.querySelectorAll(".boton");
 
 
 crearArraySeleccion (bd, seleccionAnimales);
-seleccionLetra (letraPantalla);
+seleccionLetra ();
 cargarBotones (); 
 
 // Aquí nos ponemos en la escucha de los botones
 
-console.log (botones)
 
-botones.forEach((boton) => {
-    console.log("haciendo un foreach de toda la vida");
-    boton.addEventListener("click", () => {
-        // Obtiene el ID del botón clickeado
-        console.log("clicando dentro de la funcion")
-        const idBoton = boton.querySelector("img").id;
-        // Asigna la letra del ID a la variable
-        letraSeleccionada = idBoton;
-        console.log(`Letra seleccionada: ${letraSeleccionada}`);
-        // Aquí puedes realizar otras acciones con la letra seleccionada
-    });
-});
 
 // Parte de las funciones
 
@@ -47,7 +34,7 @@ function crearArraySeleccion (bd, seleccionAnimales) {
     
 }
 
-function seleccionLetra (letraPantalla) {
+function seleccionLetra () {
     const indiceLetra = Math.floor(Math.random() * 5);
     letraPantalla = seleccionAnimales[indiceLetra].inicial;
     console.log(letraPantalla)
@@ -63,4 +50,31 @@ function cargarBotones () {
     <button class="boton boton4"> <img id="${seleccionAnimales[3].inicial}" src="${seleccionAnimales[3].url}" alt="${seleccionAnimales[3].animal}"> </button>
     <button class="boton boton5"> <img id="${seleccionAnimales[4].inicial}" src="${seleccionAnimales[4].url}" alt="${seleccionAnimales[4].animal}"> </button>
     `
+    const botones = document.querySelectorAll(".boton");
+    console.log(botones);
+
+    botones.forEach((boton) => {
+            boton.addEventListener("click", () => {
+            // Obtiene el ID del botón clickeado
+            const idBoton = boton.querySelector("img").id;
+            // Asigna la letra del ID a la variable
+            letraSeleccionada = idBoton;
+
+            console.log(`Letra seleccionada: ${letraSeleccionada}`);
+            console.log(`Letra Pantalla: ${letraPantalla}`);
+            console.log(typeof letraSeleccionada)
+            console.log(typeof letraPantalla)
+            
+            if (letraPantalla === letraSeleccionada) {
+                console.log("letra correcta");
+                boton.classList.add("acierto"); // Agrega la clase "acierto"
+            } else {
+                console.log("letra incorrecta");
+                boton.classList.add("fallo"); // Agrega la clase "fallo"
+            }
+        });
+    });
+
+    
+
 }
